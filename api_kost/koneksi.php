f972045ac62d1c1fcb5fa6d07109125387d8e511
afifahazzahraa <afifahazzahra9915@gmail.com>
2026-06-09 06:53:22 +0700

    Add files via upload

--- api_kost/koneksi.php
+++ api_kost/koneksi.php
@@ -1,12 +1,18 @@
 <?php
-$host = "sql303.infinityfree.com";
-$user = "if0_41601668";
-$pass = "apriliaudb98";
-$db   = "if0_41601668_kostdb";
+    ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+$host = "sql113.infinityfree.com";
+$user = "if0_41601435";
+$pass = "sukoharjo746";
+$db = "if0_41601435_db_kost";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
-    die("Koneksi gagal: " . mysqli_connect_error() );
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
